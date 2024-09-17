47ada986dcf4af8de4caca289e4a0db01c41e514
neolectron <xeolectron@gmail.com>
2024-09-17 08:26:19 +0200

    feat: add convars to customize gameplay experience

--- gamemodes/terrortown/entities/weapons/weapon_ttt_mutegun.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_mutegun.lua
@@ -1,5 +1,35 @@
 if SERVER then
-    AddCSLuaFile("shared.lua")
+    AddCSLuaFile("weapon_ttt_mutegun.lua")
+    resource.AddFile("materials/VGUI/ttt/icon_mute_gun.vmt")
+end
+
+--- Convars
+local traitorCanBuy = CreateConVar("ttt_mutegun_traitor", 1,
+    {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE}, "Should the Mute Gun be available for Traitors?")
+local detectiveCanBuy = CreateConVar("ttt_mutegun_detective", 1,
+    {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE}, "Should the Mute Gun be available for Detectives?")
+local muteDuration = CreateConVar("ttt_mutegun_duration", 0,
+    {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE}, "Sets mute duration in seconds, 0 for the whole round")
+local ammoCount = CreateConVar("ttt_mutegun_ammo", 1, {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE},
+    "Sets amount of ammo for the Mute Gun")
+local autoSpawnable = CreateConVar("ttt_mutegun_autospawn", 0,
+    {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE}, "Should the Mute Gun spawn automatically?")
+local shouldTraitorHearMuted = CreateConVar("ttt_mutegun_traitor_hear_muted", 1,
+    {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE}, "Should traitors hear muted players?")
+local damageOnShoot = CreateConVar("ttt_mutegun_damage", 1, {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE},
+    "How much damage it deals to the target?")
+
+--- Config logic
+local durationMessage = muteDuration:GetInt() > 0 and "for " .. muteDuration:GetInt() .. " seconds." or
+                            "for the whole round."
+
+local canBuy = {}
+if traitorCanBuy:GetBool() and detectiveCanBuy:GetBool() then
+    canBuy = {ROLE_TRAITOR, ROLE_DETECTIVE}
+elseif traitorCanBuy:GetBool() then
+    canBuy = {ROLE_TRAITOR}
+elseif detectiveCanBuy:GetBool() then
+    canBuy = {ROLE_DETECTIVE}
 end
 
 --- SWEP Info
@@ -14,7 +44,9 @@ if CLIENT then
     -- Text shown in the equip menu
     SWEP.EquipMenuData = {
         type = "Weapon",
-        desc = "Shoot someone to mute them.\nOnly traitors can hear muted players."
+        -- if duration is 0, it will mute for the whole round
+        desc = "Shoot someone to mute them.\nOnly traitors can hear muted players.\nHas " .. ammoCount:GetInt() ..
+            " shots.\nMutes the target " .. durationMessage
     };
 end
 
@@ -23,73 +55,33 @@ SWEP.HoldType = "pistol"
 
 SWEP.Primary.Ammo = "AR2AltFire"
 SWEP.Primary.Recoil = 4
-SWEP.Primary.Damage = 1
+SWEP.Primary.Damage = damageOnShoot:GetInt()
 SWEP.Primary.Delay = 2.0
 SWEP.Primary.Cone = 0.01
-SWEP.Primary.ClipSize = 1
+SWEP.Primary.ClipSize = ammoCount:GetInt()
 SWEP.Primary.Automatic = false
-SWEP.Primary.DefaultClip = 1
-SWEP.Primary.ClipMax = 1
+SWEP.Primary.DefaultClip = ammoCount:GetInt()
+SWEP.Primary.ClipMax = ammoCount:GetInt()
 SWEP.Primary.Sound = Sound("Weapon_USP.SilencedShot")
-
---- TTT config values
-
-SWEP.Kind = WEAPON_EQUIP1
-SWEP.CanBuy = {ROLE_TRAITOR} -- only traitors can buy
-SWEP.LimitedStock = true -- only buyable once
-SWEP.WeaponID = AMMO_MUTEGUN
-
-SWEP.Tracer = "AR2Tracer"
-
 SWEP.UseHands = true
 SWEP.ViewModel = Model("models/weapons/c_357.mdl")
 SWEP.WorldModel = Model("models/weapons/w_357.mdl")
 
--- If AutoSpawnable is true and SWEP.Kind is not WEAPON_EQUIP1/2, then this gun can
--- be spawned as a random weapon. Of course this AK is special equipment so it won't,
--- but for the sake of example this is explicitly set to false anyway.
-SWEP.AutoSpawnable = false
-
--- The AmmoEnt is the ammo entity that can be picked up when carrying this gun.
-SWEP.AmmoEnt = "item_ammo_smg1_ttt"
-
--- InLoadoutFor is a table of ROLE_* entries that specifies which roles should
--- receive this weapon as soon as the round starts. In this case, none.
-SWEP.InLoadoutFor = nil
-
--- If LimitedStock is true, you can only buy one per round.
+--- TTT SWEP Info
+SWEP.Kind = WEAPON_EQUIP1
+SWEP.CanBuy = canBuy
 SWEP.LimitedStock = false
-
--- If AllowDrop is false, players can't manually drop the gun with Q
-SWEP.AllowDrop = true
-
--- If IsSilent is true, victims will not scream upon death.
+SWEP.Tracer = "AR2Tracer"
+SWEP.AutoSpawnable = autoSpawnable:GetBool()
 SWEP.IsSilent = true
 
--- If NoSights is true, the weapon won't have ironsights
-SWEP.NoSights = false
-
--- Tell the server that it should download our icon to clients.
-if SERVER then
-    resource.AddFile("materials/VGUI/ttt/icon_mute_gun.vmt")
-end
-
---- Utilities
-
--- Table to keep track of muted players
+--- Hooks
 local mutedPlayers = {}
 
-function MutePlayer(ply, target)
-    mutedPlayers[target] = true
-    print("Muting player " .. target:Nick())
-end
-
---- Hooks / events
-
--- Hook to control who can hear who
+-- Handle player voice chat muting
 hook.Add("PlayerCanHearPlayersVoice", "HandlePlayerMute", function(listener, talker)
-    -- If the listener is the traitor, they can hear everyone
-    if listener:IsTraitor() then
+    -- If the listener is the traitor, can he hear muted players?
+    if listener:IsTraitor() and shouldTraitorHearMuted:GetBool() then
         return true
     end
 
@@ -101,15 +93,13 @@ end)
 
 -- Reset all muted players on round end
 hook.Add("TTTEndRound", "ResetMutedPlayers", function()
-    mutedPlayers = {} -- Clear the muted players table
-    print("Reset all muted players.")
+    mutedPlayers = {}
 end)
 
 -- Unmute player on death
 hook.Add("PlayerDeath", "UnmuteOnDeath", function(victim)
     if mutedPlayers[victim] then
-        mutedPlayers[victim] = nil -- Remove from muted players table
-        print("Unmuting player " .. victim:Nick() .. " on death.")
+        mutedPlayers[victim] = nil
     end
 end)
 
@@ -121,8 +111,43 @@ hook.Add("PlayerSay", "BlockChatForMutedPlayers", function(ply, text)
     end
 end)
 
+--- SWEP Functions
+function MuteTarget(target, path, dmginfo)
+    local ent = path.Entity
+
+    if not IsValid(ent) or not ent:IsPlayer() then
+        return
+    end
+
+    -- Add the target to the muted players list
+    mutedPlayers[ent] = true
+
+    -- Unmute after the specified duration
+    if muteDuration:GetInt() > 0 then
+        timer.Simple(muteDuration:GetInt(), function()
+            mutedPlayers[ent] = nil
+        end)
+    end
+
+end
+
+function SWEP:ShootMute()
+    local cone = self.Primary.Cone
+    local bullet = {}
+    bullet.Num = 1
+    bullet.Src = self.Owner:GetShootPos()
+    bullet.Dir = self.Owner:GetAimVector()
+    bullet.Spread = Vector(cone, cone, 0)
+    bullet.Tracer = 1
+    bullet.Force = 2
+    bullet.Damage = self.Primary.Damage
+    bullet.TracerName = self.Tracer
+    bullet.Callback = MuteTarget
+
+    self.Owner:FireBullets(bullet)
+end
+
 function SWEP:PrimaryAttack()
-    -- handle delay (see above)
     self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
 
     if not self:CanPrimaryAttack() then
@@ -131,6 +156,7 @@ function SWEP:PrimaryAttack()
 
     self:EmitSound(self.Primary.Sound)
     self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+    self:ShootMute()
     self:TakePrimaryAmmo(1)
 
     -- recoil and animation on the client
@@ -140,19 +166,6 @@ function SWEP:PrimaryAttack()
         self:GetOwner():ViewPunch(Angle(math.Rand(-0.2, -0.1) * self.Primary.Recoil,
             math.Rand(-0.1, 0.1) * self.Primary.Recoil, 0))
     end
-
-    -- get owner and target
-    local owner = self:GetOwner()
-    local tr = owner:GetEyeTrace(MASK_SHOT)
-    local ent = tr.Entity
-
-    -- check if the target is a player
-    if not IsValid(ent) or not ent:IsPlayer() then
-        return
-    end
-
-    -- Mute the target for everyone except the traitor
-    MutePlayer(owner, ent)
 
 end
 
